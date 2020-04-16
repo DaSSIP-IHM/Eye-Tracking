28f3f95cace8442782d01c9c01671d27e73d1536
Alphonse Terrier <alphonse.terrier@isep.fr>
2020-04-16 22:23:09 +0200

    Update detectors.py

--- components/detectors.py
+++ components/detectors.py
@@ -1,113 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of PyGaze - the open-source toolbox for eye tracking
-#
-#	PyGazeAnalyser is a Python module for easily analysing eye-tracking data
-#	Copyright (C) 2014  Edwin S. Dalmaijer
-#
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>
-
-# EyeTribe Reader
-#
-# Reads files as produced by PyTribe (https://github.com/esdalmaijer/PyTribe),
-# and performs a very crude fixation and blink detection: every sample that
-# is invalid (usually coded '0.0') is considered to be part of a blink, and
-# every sample in which the gaze movement velocity is below a threshold is
-# considered to be part of a fixation. For optimal event detection, it would be
-# better to use a different algorithm, e.g.:
-# Nystrom, M., & Holmqvist, K. (2010). An adaptive algorithm for fixation,
-# saccade, and glissade detection in eyetracking data. Behavior Research
-# Methods, 42, 188-204. doi:10.3758/BRM.42.1.188
-#
-# (C) Edwin Dalmaijer, 2014
-# edwin.dalmaijer@psy.ox.ax.uk
-#
-# version 1 (01-Jul-2014)
-
-__author__ = "Edwin Dalmaijer"
-
 import numpy
 
 
-def blink_detection(x, y, time, missing=0.0, minlen=10):
-    """Detects blinks, defined as a period of missing data that lasts for at
-    least a minimal amount of samples
-
-    arguments
-
-    x		-	numpy array of x positions
-    y		-	numpy array of y positions
-    time		-	numpy array of EyeTribe timestamps
-
-    keyword arguments
-
-    missing	-	value to be used for missing data (default = 0.0)
-    minlen	-	integer indicating the minimal amount of consecutive
-                missing samples
-
-    returns
-    Sblk, Eblk
-                Sblk	-	list of lists, each containing [starttime]
-                Eblk	-	list of lists, each containing [starttime, endtime, duration]
-    """
-
-    # empty list to contain data
-    Sblk = []
-    Eblk = []
-
-    # check where the missing samples are
-    mx = numpy.array(x == missing, dtype=int)
-    my = numpy.array(y == missing, dtype=int)
-    miss = numpy.array((mx + my) == 2, dtype=int)
-
-    # check where the starts and ends are (+1 to counteract shift to left)
-    diff = numpy.diff(miss)
-    starts = numpy.where(diff == 1)[0] + 1
-    ends = numpy.where(diff == -1)[0] + 1
-
-    # compile blink starts and ends
-    for i in range(len(starts)):
-        # get starting index
-        s = starts[i]
-        # get ending index
-        if i < len(ends):
-            e = ends[i]
-        elif len(ends) > 0:
-            e = ends[-1]
-        else:
-            e = -1
-        # append only if the duration in samples is equal to or greater than
-        # the minimal duration
-        if e - s >= minlen:
-            # add starting time
-            Sblk.append([time[s]])
-            # add ending time
-            Eblk.append([time[s], time[e], time[e] - time[s]])
-
-    return Sblk, Eblk
-
-
-def remove_missing(x, y, time, missing):
-    mx = numpy.array(x == missing, dtype=int)
-    my = numpy.array(y == missing, dtype=int)
-    x = x[(mx + my) != 2]
-    y = y[(mx + my) != 2]
-    time = time[(mx + my) != 2]
-    return x, y, time
-
-
-def fixation_detection(x, y, time, missing=0.0, maxdist=25, mindur=50):
+def fixation_detection(x, y, time, maxdist=25, mindur=50):
     """Detects fixations, defined as consecutive samples with an inter-sample
     distance of less than a set amount of pixels (disregarding missing data)
 
@@ -129,14 +23,17 @@ def fixation_detection(x, y, time, missing=0.0, maxdist=25, mindur=50):
     Sfix, Efix
                 Sfix	-	list of lists, each containing [starttime]
                 Efix	-	list of lists, each containing [starttime, endtime, duration, endx, endy]
+
+    author : Edwin Dalmaijer
+
+    Add of pupil dilatation mean by Alphonse Terrier
     """
 
-    #x, y, time = remove_missing(x, y, time, missing)
+    # x, y, time = remove_missing(x, y, time, missing)
 
     # empty list to contain data
     Sfix = []
     Efix = []
-
 
     # loop through all coordinates
     si = 0
