96004966aba7ae0205ec9d81fec17362cf537928
Alphonse Terrier <alphonse.terrier@isep.fr>
2020-04-23 16:05:19 +0200

    Update example.py

--- components/example.py
+++ components/example.py
@@ -7,7 +7,7 @@ FILENAME = 'all_gaze_data-958009508617'
 IMAGENAME = 'liberte1080.jpg'
 
 
-def process_fixations(df):
+def process_fixations(df, maxdist=175, mindur=2000):
     print(df.shape)
     df = df[(df['left_gaze_point_validity'] == 1) | (df['right_gaze_point_validity'] == 1)].reset_index()
     print(df.shape)
@@ -20,9 +20,9 @@ def process_fixations(df):
     # for maxdist in [100, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400]:
     # Sfix, Efix = old_fixation_detection(df['x'], df['y'], df['system_time_stamp'], maxdist=maxdist, mindur=2000)
     # print(Sfix)
-    maxdist = 175
+
     Sfix, Efix = fixation_detection(df['x'], df['y'], df['mean_pupil_diameter'], df['system_time_stamp'],
-                                    maxdist=maxdist, mindur=2000)
+                                    maxdist=maxdist, mindur=mindur)
 
     df_fixations = pd.DataFrame(data=Efix, columns=['starttime', 'endtime', 'duration', 'x', 'y', 'dilatation'])
 
@@ -32,23 +32,23 @@ def process_fixations(df):
     return df_fixations
 
 
-def process_one_image(filename=FILENAME, imagename=IMAGENAME, default_path=''):
+def process_one_image(filename=FILENAME, imagename=IMAGENAME, default_path='', maxdist=175, mindur=2000):
     df = pd.read_excel(default_path + r'data/' + filename + '.xlsx')
 
-    df_fixations = process_fixations(df)
+    df_fixations = process_fixations(df, maxdist, mindur)
 
     df_fixations.to_excel(default_path + r'processed_data/' + filename + '-fixations.xlsx', index=False)
 
     # plot_path(df['x'], df['y'], imagename, linewidth=0.3, markersize=0.6, default_path=default_path)
     plotly_fixations_points(df_fixations, imagename,
                             default_path=default_path,
-                            output_ind=str(175))
+                            output_ind=str(maxdist))
 
 
-def process_many_images(filename=FILENAME, default_path=''):
+def process_many_images(filename=FILENAME, default_path='', maxdist=175, mindur=2000):
     df = pd.read_excel(default_path + r'data/' + filename + '.xlsx')
 
-    df_fixations = process_fixations(df)
+    df_fixations = process_fixations(df, maxdist, mindur)
     df_fixations.to_excel(default_path + r'processed_data/' + filename + '-fixations.xlsx', index=False)
 
     df_image_acquisition = df[df['image_acquisition'] == True]
