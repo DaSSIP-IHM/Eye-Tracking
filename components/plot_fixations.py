00aa1abf2a38a063a414c193d6b2366d77541e26
Alphonse Terrier <alphonse.terrier@isep.fr>
2020-04-23 19:26:55 +0200

    Correction videowriter

--- components/plot_fixations.py
+++ components/plot_fixations.py
@@ -26,7 +26,7 @@ def plotly_fixations_points(df, image_name, default_path='', output_ind=''):
     # print(duration.sum)
 
     hovertext = '<b>Durée</b> : ' + df['duration'].round(2).astype(str) + 's<br><b>Moyenne de dilatation</b> :' + df[
-        'dilatation'].round(2).astype(str) + 'mm<br><b>Fixation n°</b> ' + (df.index+1).astype(str)
+        'dilatation'].round(2).astype(str) + 'mm<br><b>Fixation n°</b> ' + (df.index + 1).astype(str)
 
     fig.add_trace(
         go.Scatter(x=df['x'], y=df['y'], mode='markers', marker_size=hoverduration, hoverinfo="text",
@@ -34,7 +34,7 @@ def plotly_fixations_points(df, image_name, default_path='', output_ind=''):
     )
 
     fig.add_trace(
-        go.Scatter(x=df['x'], y=df['y'],hoverinfo='skip', marker_size=3, line=dict(width=1))
+        go.Scatter(x=df['x'], y=df['y'], hoverinfo='skip', marker_size=3, line=dict(width=1))
     )
     fig.update_layout(title='Affichage des fixations',
                       xaxis=dict(range=[0, x_res], showgrid=False),
@@ -60,26 +60,27 @@ def plotly_fixations_points(df, image_name, default_path='', output_ind=''):
 
 
 def matplotlib_fixations_points(df_fixations, system_time_stamp, filename, default_path=''):
-    image_name = default_path + 'images/' + filename+r'/'+str(system_time_stamp)+'.png'
-    #print(image_name)
+    temp_df = df_fixations[
+        (df_fixations['starttime'] <= system_time_stamp) & (df_fixations['endtime'] > system_time_stamp)]
+
+    image_name = default_path + 'images/' + filename + r'/' + str(system_time_stamp) + '.png'
 
     im = plt.imread(image_name)
     plt.imshow(im)
-
-    temp_df = df_fixations[(df_fixations['starttime'] <= system_time_stamp) & (df_fixations['endtime'] > system_time_stamp)]
     print(temp_df)
     plt.scatter(temp_df['x'], temp_df['y'], c='#ff876f', s=temp_df['norm_dilatation'], alpha=0.4)
 
-    output_name = default_path + 'processed_images/' + filename+r'/'+str(system_time_stamp)+'.png'
+    output_name = default_path + 'processed_images/' + filename + r'/' + str(system_time_stamp) + '.png'
     plt.savefig(output_name, dpi=300)
     plt.clf()
 
-def export_video(filename, default_path='', fps=30):
-    path = default_path + 'processed_images/'+ filename
 
-    img_list=[]
+def export_video(filename, default_path='', fps=30):
+    path = default_path + 'processed_images/' + filename
+
+    img_list = []
     for picname in os.listdir(path):
-        path_file = path +r'/'+ picname
+        path_file = path + r'/' + picname
         print(path_file)
 
         img = cv2.imread(path_file)
@@ -87,7 +88,8 @@ def export_video(filename, default_path='', fps=30):
         size = (width, height)
         img_list.append(img)
 
-    out = cv2.VideoWriter(default_path+'processed_videos/'+filename+'.avi', cv2.VideoWriter_fourcc(*'DIVX'), fps=fps, fourcc=size)
+    out = cv2.VideoWriter(default_path + 'processed_videos/' + filename + '.avi', cv2.VideoWriter_fourcc(*'DIVX'),
+                          fps=fps, frameSize=size)
 
     for i in range(len(img_list)):
         out.write(img_list[i])
